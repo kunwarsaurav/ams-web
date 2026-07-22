ee9e2b075946ae9168f44bafb68e09c09f24dc78
Saurav Kunwar <kunwarsaurav10@Gmail.com>
2026-07-22 13:19:05 +0545

    removed unwanted files

--- backend/test.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-from urllib.error import HTTPError
-try:
-    urllib.request.urlopen('http://127.0.0.1:8080/attendance/export?start_date=2026-06-30&end_date=2026-07-07')
-except HTTPError as e:
-    print(e.read().decode())
--- backend/test_export.py
+++
@@ -1,9 +0,0 @@
-import traceback
-from datetime import date
-from app.database.database import SessionLocal
-from app.api.attendance import export_attendance_excel
-db=SessionLocal()
-try:
-    export_attendance_excel(date(2026, 6, 30), date(2026, 7, 7), db)
-except Exception as e:
-    traceback.print_exc()
